fcd97bf2e585ba60d8f0c036cfc49ff244860cd3
Aman Gupta <aman930530@gmail.com>
2022-07-24 02:21:36 +0530

    MAKE : whole website responsive

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,14 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
-import { GiBugleCall } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
+import { RiMenu3Fill } from "react-icons/ri";
 import { Link } from "react-scroll";
 
 const Header = () => {
   const menu = ["Home", "Works", "Projects", "Sevices"];
+  const [checked, setChecked] = useState(false);
   return (
-    <div className="header_container">
-      <div className="header_logo">Personal.</div>
-      <div className="header_menu">
+    <>
+      <div className="header_container">
+        <div className="header_logo">Personal.</div>
+        <div className="header_menu">
+          {/* <input
+            className="menu-btn"
+            type="checkbox"
+            id="menu-btn"
+            defaultChecked={checked}
+            onChange={() => setChecked((data) => !data)}
+          />
+          <label className="menu-icon" htmlFor="menu-btn">
+            <span
+              className={ "navicon"}
+            ></span>
+          </label> */}
+          {!checked ? (
+            <RiMenu3Fill
+              className="menu-icon"
+              onClick={() => setChecked((data) => !data)}
+            />
+          ) : (
+            <GrClose
+              className="menu-icon"
+              onClick={() => setChecked((data) => !data)}
+            />
+          )}
+          <ul className="navbar">
+            {menu.map((item, key) => (
+              <Link
+                activeClass="active"
+                key={key}
+                to={item}
+                smooth={true}
+                duration={800}
+                offset={-80}
+                spy={true}
+              >
+                {item}
+              </Link>
+            ))}
+          </ul>
+        </div>
+        {/* <div className="header_Button">
+        Contact us
+        <GiBugleCall size={20} color="black" />
+      </div> */}
+      </div>
+      <div
+        style={{
+          transform: !checked && "translateY(-200%)",
+        }}
+        className={"header_line"}
+      >
         <ul className="navbar">
           {menu.map((item, key) => (
             <Link
@@ -19,17 +72,14 @@ const Header = () => {
               duration={800}
               offset={-80}
               spy={true}
+              onClick={() => setChecked((data) => !data)}
             >
               {item}
             </Link>
           ))}
         </ul>
       </div>
-      <div className="header_Button">
-        Contact us
-        <GiBugleCall size={20} color="black" />
-      </div>
-    </div>
+    </>
   );
 };
 
